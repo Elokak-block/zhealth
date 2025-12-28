81297dc1b8c2d2d38ce6257e9cf84860f8003b56
Stellamaris Nwobodo <stellamarisnwo6@gmail.com>
2025-12-28 15:14:11 +0000

    also make the whatsapp same color with others

--- src/components/results-client.tsx
+++ src/components/results-client.tsx
@@ -199,7 +199,7 @@ function ResultsClientInternal({ data }: { data?: string }) {
                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 fill-current"><title>X</title><path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/></svg>
                 </Button>
                 <Button variant="outline" size="icon" onClick={shareOnWhatsApp} aria-label="Share on WhatsApp">
-                   <Image src="/whatsapp.png" alt="WhatsApp" width={20} height={20} />
+                   <Image src="/whatsapp.png" alt="WhatsApp" width={20} height={20} className="filter grayscale brightness-200" />
                 </Button>
                  <Button variant="outline" size="icon" onClick={shareOnFacebook} aria-label="Share on Facebook">
                    <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 fill-current"><title>Facebook</title><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
