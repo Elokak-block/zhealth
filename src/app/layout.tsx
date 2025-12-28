751b8be9f1409255f7ebef91106a37bee62edd25
Stellamaris Nwobodo <stellamarisnwo6@gmail.com>
2025-12-28 16:28:31 +0000

    change the googl tag for analytics: Below is the Google tag for this acc

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,13 +19,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-J8XCZCL7TV"
+        ></Script>
+        <Script id="google-analytics">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-582CWQXW');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-J8XCZCL7TV');
           `}
         </Script>
         <meta name="google-adsense-account" content="ca-pub-9498357581912172" />
