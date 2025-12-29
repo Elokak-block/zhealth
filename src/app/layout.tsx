de48efb3acec1a03be27cf4f6368d49ddab327d5
Stellamaris Nwobodo <stellamarisnwo6@gmail.com>
2025-12-29 09:51:51 +0000

    ## Error Type Console Error
    ## Error Message
    visit_uuid not found for E

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,21 +19,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
-        <Script data-cfasync="false" src="https://cmp.gatekeeperconsent.com/min.js" strategy="beforeInteractive" />
-        <Script id="ezoic-integration" strategy="beforeInteractive">
-          {`
-            window.ezstandalone = window.ezstandalone || {};
-            ezstandalone.cmd = ezstandalone.cmd || [];
-          `}
-        </Script>
-        <Script async src="//www.ezojs.com/ezoic/sa.min.js" strategy="beforeInteractive" />
-        <Script id="google-tag-manager">
+        <Script id="google-tag-manager" strategy="beforeInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-582CWQXW');`}
         </Script>
+        <Script id="ezoic-integration" strategy="beforeInteractive">
+          {`
+            window.ezstandalone = window.ezstandalone || {};
+            ezstandalone.cmd = ezstandalone.cmd || [];
+          `}
+        </Script>
+        <Script src="//www.ezojs.com/ezoic/sa.min.js" strategy="beforeInteractive" />
       </head>
       <body
         className={cn(
