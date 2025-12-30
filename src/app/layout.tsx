c8328871031210d7e039f5ff6125efe754150611
Stellamaris Nwobodo <stellamarisnwo6@gmail.com>
2025-12-30 14:44:26 +0000

    <meta name="monetag" content="4253c72bad5c9b56283a33d7102d7059"> Add thi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark" suppressHydrationWarning>
       <head>
+        <meta name="monetag" content="4253c72bad5c9b56283a33d7102d7059" />
       </head>
       <body
         className={cn(
